3d2cb0c1290345dc86c3ab8c593f37a0f259ff4c
Joseph-Gamboa-36 <gamboa0639@gmail.com>
2026-06-11 11:18:35 -0500

    Actualización

---
+++ JS/Objeto document.js
@@ -0,0 +1,21 @@
+
+// Por selector CSS - primer elemento que coincida
+const primerParrafo = document.querySelector("p");
+primerParrafo.innerHTML = "Este es un parrafo desde JS"
+
+// // Por ID (devuelve un solo elemento)
+// const titulo = document.getElementById("titulo-principal");
+
+// // Por selector CSS - primer elemento que coincida
+// const primerParrafo = document.querySelector("p");
+// const cajaPrincipal = document.querySelector(".caja");
+// const botonPorId = document.querySelector("#mi-boton");
+
+// // Todos los elementos que coincidan (devuelve una lista)
+// const todosLosParrafos = document.querySelectorAll("p");
+// const todasLasCajas = document.querySelectorAll(".caja");
+
+// // Recorrer la lista con forEach
+// todosLosParrafos.forEach((p) => {
+// console.log(p.textContent);
+// });
