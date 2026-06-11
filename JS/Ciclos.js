0e650e7b5773f70b85ec112aaa55d59763ec1bc4
Joseph-Gamboa-36 <gamboa0639@gmail.com>
2026-06-11 11:17:02 -0500

    Creación de Object document

--- JS/Ciclos.js
+++ JS/Ciclos.js
@@ -1,10 +1,6 @@
-let i = 5;
-for (i; i > 1; i--) {
-    console.log(`Iteración: ${i}`);
-}
-
-// Tabla del 7
-const tabla = 7;
-for (let i = 1; i <= 10; i++) {
-    console.log(`${tabla} x ${i} = ${tabla * i}`);
-}
+let contrasena
+let clave = 123;
+do {
+    contrasena = parseInt(prompt("Ingrese su contraseña"));
+    console.log(`Contraseña incorrecta: ${contrasena}`)
+} while (contrasena !== clave);
