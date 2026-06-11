2c3fd3d97262e19b4bfa8051299dbcd6e8d3dfdf
Joseph-Gamboa-36 <gamboa0639@gmail.com>
2026-06-11 06:25:39 -0500

    Actualizado reto 5

--- JS/Reto 5.js
+++ JS/Reto 5.js
@@ -2,16 +2,21 @@ let nota = parseInt(prompt("Ingresa una nota"));
 let Aprobado;
 if (nota >= 0&& nota <=69) {
     Aprobado = "No Aprobado";
+    console.log(Aprobado);
 } else if (nota >= 70&& nota <=79) {
     Aprobado = "Aprobado";
+    console.log(Aprobado);
 } else if (nota >= 80&& nota <= 89) {
     Aprobado = "Notable";
+    console.log(Aprobado);
 } else if (nota > 90&& nota <= 100) {
     Aprobado = "Sobresaliente";
+    console.log(Aprobado);
 }
 if (nota > 100) {
     Aprobado = "Nota Inválida";
+    console.log(Aprobado);
 } else if (nota < 0) {
     Aprobado = "Nota Inválida";
+    console.log(Aprobado);
 }
-console.log(`Con ${nota} de nota, estas: ${Aprobado}`);
