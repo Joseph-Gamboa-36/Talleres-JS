a63048d0a31934007f181bb4d5259023988420e8
Joseph-Gamboa-36 <gamboa0639@gmail.com>
2026-06-11 09:22:20 -0500

    Esctructuras de control

--- JS/Ciclos.js
+++ JS/Ciclos.js
@@ -1,4 +1,10 @@
-for(let i = 0; i < 5; i++){
+let i = 5;
+for (i; i > 1; i--) {
     console.log(`Iteración: ${i}`);
 }
 
+// Tabla del 7
+const tabla = 7;
+for (let i = 1; i <= 10; i++) {
+    console.log(`${tabla} x ${i} = ${tabla * i}`);
+}
